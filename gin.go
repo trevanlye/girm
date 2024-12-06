b8f57093d6e7973cad26415f2669218fed004382
李春风 <305236917.qq.com>
2024-12-06 14:14:41 +0800

    再次提交

--- gin.go
+++ gin.go
@@ -1,1 +1,61 @@
 package girm
+
+import (
+	"bytes"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+)
+
+// Compatible with single object or multiple objects
+func operation[T any](c *gin.Context, opr func(...*T) error) {
+	var err error
+	var rsp any
+	defer func() {
+		if err != nil {
+			jsonFail(c, err.Error())
+		} else {
+			jsonOK(c, rsp)
+		}
+	}()
+	jsonData, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		return
+	}
+	//write jsonData back to request body
+	c.Request.Body = io.NopCloser(bytes.NewBuffer(jsonData))
+
+	if jsonData[0] == '[' { //array
+		var es []*T
+		if err = c.ShouldBindJSON(&es); err != nil {
+			return
+		}
+		if err = opr(es...); err != nil {
+			return
+		}
+		rsp = es
+	} else { //object
+		var e T
+		if err = c.ShouldBindJSON(&e); err != nil {
+			return
+		}
+		if err = opr(&e); err != nil {
+			return
+		}
+		rsp = e
+	}
+}
+
+func jsonOK(c *gin.Context, obj any) {
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": obj,
+	})
+}
+
+func jsonFail(c *gin.Context, obj any) {
+	c.JSON(http.StatusOK, gin.H{
+		"code":    500,
+		"message": obj,
+	})
+}
--- girm.go
+++ girm.go
@@ -1,1 +1,103 @@
 package girm
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"strconv"
+)
+
+type DB[T any] struct {
+	db *gorm.DB
+}
+
+func NewDb[T any](db *gorm.DB) *DB[T] {
+	return &DB[T]{
+		db: db,
+	}
+}
+
+func (d *DB[T]) Insert(c *gin.Context) {
+	operation(c, func(es ...*T) error {
+		return bulkOperate(es, d.db.Create)
+	})
+}
+
+func (d *DB[T]) SelectAll(c *gin.Context) {
+	var es []*T
+	if err := d.db.Find(&es).Error; err != nil {
+		jsonFail(c, err.Error())
+		return
+	}
+	jsonOK(c, es)
+}
+
+func (d *DB[T]) SelectByPage(c *gin.Context, where func(db *gorm.DB) *gorm.DB) {
+	var err error
+	page := struct {
+		PageNum  int
+		PageSize int
+		Total    int
+		Data     []*T
+	}{
+		PageNum: 1,
+	}
+
+	defer func() {
+		if err != nil {
+			jsonFail(c, err.Error())
+		} else {
+			jsonOK(c, &page)
+		}
+	}()
+
+	sPageSize := c.Query("pageSize")
+	pageSize, err := strconv.Atoi(sPageSize)
+	if err != nil || pageSize <= 0 {
+		return
+	}
+	page.PageSize = pageSize
+
+	sPageNum := c.Query("pageNum")
+	pageNum, err := strconv.Atoi(sPageNum)
+	if err != nil || pageNum <= 0 {
+		return
+	}
+	page.PageNum = pageNum
+
+	var count int64
+	offset := (pageNum - 1) * pageSize
+	if where != nil {
+		where(d.db.Model(new(T))).Count(&count)
+		where(d.db).Offset(offset).Limit(pageSize).Find(&page.Data)
+	} else {
+		d.db.Model(new(T)).Count(&count)
+		d.db.Offset(offset).Limit(pageSize).Find(&page.Data)
+	}
+
+	page.Total = int(count)
+}
+
+func (d *DB[T]) Save(c *gin.Context) {
+	operation(c, func(es ...*T) error {
+		return bulkOperate(es, d.db.Save)
+	})
+}
+
+func (d *DB[T]) Delete(c *gin.Context) {
+	var ids []int
+	if err := c.ShouldBindJSON(&ids); err != nil {
+		jsonFail(c, err.Error())
+		return
+	}
+
+	var es []*T
+	if err := d.db.Where("id IN ?", ids).Find(&es).Error; err != nil {
+		jsonFail(c, err.Error())
+		return
+	}
+	if err := d.db.Delete(&es).Error; err != nil {
+		jsonFail(c, err.Error())
+		return
+	}
+	jsonOK(c, nil)
+}
